4f1ef7229b0654301afa410ae1b5320606919498
whl <whlit.cola@gmail.com>
2024-11-07 22:24:40 +0800

    fix: help

--- src/main.go
+++ src/main.go
@@ -55,14 +55,14 @@ func help() {
 	fmt.Println("  jdk                      jdk版本管理")
 	fmt.Println("  maven                    maven版本管理")
 	fmt.Println("  node                     node版本管理")
-	fmt.Println("  <name>                   用create创建的其他版本管理的名称")
+	fmt.Println("  <name>                   其他自定义的版本管理，可在配置文件中手动添加")
 	fmt.Println("Actions:")
-	fmt.Println("  create                   创建一个自定义版本管理")
 	fmt.Println("  add <version> <path>     添加版本,version: 版本名称(自定义),path: 版本的绝对路径")
 	fmt.Println("  rm                       移除版本")
 	fmt.Println("  list                     查询所有已添加的版本管理")
 	fmt.Println("  use                      使用版本")
 	fmt.Println("  install                  在线安装新版本,自定义的版本管理不支持")
+    fmt.Println("  init                     初始化环境变量,创建完成后需要重启命令行才能生效")
 }
 
 func initManagers() {
